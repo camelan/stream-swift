f43d84cf97f3397201c18b83aa2f211c30ec39d8
Sherif <sherif.shoukry@icloud.com>
2023-04-20 12:41:55 +0200

    update Swime Package

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "14.0.1")),
         .package(url: "https://github.com/daltoniam/Starscream.git", .upToNextMajor(from: "4.0.4")),
-        .package(url: "https://github.com/sendyhalim/Swime", .upToNextMajor(from: "3.1.0")),
+        .package(url: "https://github.com/camelan/Swime", .upToNextMajor(from: "3.1.1")),
     ],
     targets: [
-        .target(name: "GetStream", dependencies: ["Moya", "Faye", "Swime"], path: "Sources", exclude: ["Token"]),
+        .target(name: "GetStream", dependencies: ["Moya", "Faye", .product(name: "Swime", package: "Swime")], path: "Sources", exclude: ["Token"]),
         .target(name: "Faye", dependencies: ["Starscream"], path: "Faye"),
         .testTarget(name: "GetStreamTests", dependencies: ["GetStream"], path: "Tests", exclude: ["Token"]),
     ]
