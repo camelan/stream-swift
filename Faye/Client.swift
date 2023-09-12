1a0aace80fc2a8728d7a00039f82826b7ca86527
Sherif <sherif.shoukry@icloud.com>
2023-09-12 10:20:22 +0300

    handle websocket delegate

--- Faye/Client.swift
+++ Faye/Client.swift
@@ -141,9 +141,7 @@ extension Client {
 // MARK: - Connection
 
 extension Client: WebSocketDelegate {
-    public func didReceive(event: Starscream.WebSocketEvent, client: Starscream.WebSocketClient) {}
-    
-    public func didReceive(event: WebSocketEvent, client: WebSocket) {
+    public func didReceive(event: Starscream.WebSocketEvent, client: Starscream.WebSocketClient) {
         switch event {
         case .connected(let headers):
             isWebSocketConnected = true
